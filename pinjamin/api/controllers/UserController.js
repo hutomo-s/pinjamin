7307a05e81346a39675c8e8e63c8462d9535b6ad
Mira Pravitasari <mira.pravitasari@kudo.co.id>
2016-09-29 07:35:03 +0700

    create user

--- pinjamin/api/controllers/UserController.js
+++ pinjamin/api/controllers/UserController.js
@@ -1,2 +1,12 @@
 module.exports = {
+  createUser: function(req, res){
+    var params = req.params.all();
+
+    User.create({id: params.id, name: params.name, email: params.email, mobile:params.mobile, address:params.address,
+        username:params.username, password:params.password}).exec(function createUser(err,created){
+      return res.json({
+        notice: 'Created user ' + params.username
+      });
+    });
+  }
 };
