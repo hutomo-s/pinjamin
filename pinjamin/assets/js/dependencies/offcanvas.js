861047125aba44b9444bff17fc9e9bc4e58fd465
Ivan_Fernando <ivan@kudo.co.id>
2016-09-30 15:19:10 +0700

    masih debug

--- pinjamin/assets/js/dependencies/offcanvas.js
+++ pinjamin/assets/js/dependencies/offcanvas.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-  $('.nav-toggle-button').click(function () {
-    $('.row-offcanvas').toggleClass('active');
-    console.log('nav toggle');
+  $('[data-toggle="offcanvas"]').click(function () {
+    $('.row-offcanvas').toggleClass('active')
   });
 });
